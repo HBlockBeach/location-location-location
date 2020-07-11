e9550a9487c9eae41f50a7c0119f69b670a3d5b6
Hunter Block <hunterblock@Hunters-MacBook-Pro.local>
2020-07-11 12:04:20 -0400

    map

---
+++ static/logic.js
@@ -0,0 +1,10 @@
+var mymap = L.map('map').setView([37.09, -95.71], 4);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(mymap);
