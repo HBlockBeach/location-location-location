0531b9e449d529447ef9852863a4f7047454fd91
Hunter Block <hunterblock@Hunters-MacBook-Pro.local>
2020-07-16 10:42:49 -0400

    first

--- static/logic.js
+++ static/logic.js
@@ -6,24 +6,81 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
     id: 'streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiYmxvY2toIiwiYSI6ImNrYWVpcTBlZTAyNDcycHJ3cTgyOTMzbzIifQ.Ig5e-_6ao22OX0xPpryIEA"
 }).addTo(mymap);
 
 var zillowdata = "http://127.0.0.1:5000/data";
 
-console.log("before d3.json")
+//console.log("before d3.json")
 
-d3.json(zillowdata).then(function(housing_data) {
-    console.log("In d3.json call")
-    console.log(zillowdata);
+function layer(){
+    d3.json(zillowdata).then(function(housing_data) {
+    //console.log("In d3.json call")
+    //console.log(zillowdata);
 
-    console.log(housing_data[0]);
+    //console.log(housing_data[885]);
 
     for (var i = 0; i < 25; i++) {
         var marker = L.marker([housing_data[i][20], housing_data[i][21]]).addTo(mymap)
         .bindPopup("<p>Metropolitan Area: " + (housing_data[i][1]));
         
     }
-});
+})};
+
+layer()
+
+    function createchart2(yearinput){
+      d3.json(zillowdata).then(function(yearData) {
+          var dataForInput = yearData.filter(row => row[1].trim() == yearinput);
+          //console.log(dataForInput)
+          lat = dataForInput.map(row => row[20]);
+          lng = dataForInput.map(row => row[21]);
+          city = dataForInput.map(row => row[1]);
+          medianvalue =dataForInput.map(row=>row[19])
+
+          newlat= parseFloat(lat)
+          newlng = parseFloat(lng)
+          
+
+          var formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          });
+          
+          newmedian = formatter.format(medianvalue);
+
+          //console.log(newmedian)
+          //console.log(newlat)
+          //console.log(newlng)
+          //console.log(city)
+          
+          var marker = L.marker([newlat, newlng]).addTo(mymap)
+            .bindPopup("<p>Metropolitan Area: " + city + "<br>" + "\n" + "The median house price is " + newmedian).openPopup();
+        })}
+
+function medianvalue(input){
+  d3.json(zillowdata).then(function(update) {
+  var dataForInput = update.filter(row => row[1].trim() == input);
+ 
+  medianvalues =dataForInput.map(row=>row[19])
+  console.log(medianvalues)
+  ;
+
+})};
+
+d3.selectAll("#city").on("change", updatePage);
+function updatePage() {
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.selectAll("#city").node();
+  // Assign the dropdown menu item ID to a variable
+  var dropdownMenuID = dropdownMenu.id;
+  // Assign the dropdown menu option to a variable
+  var selectedOption = dropdownMenu.value;
+  //console.log(dropdownMenuID);
+  //console.log(selectedOption);
+  createchart2(selectedOption)
+  medianvalue(selectedOption)};
+
+
         
   
