5bd0b10e695b80b72c84401f32c52054147ee8fa
Hunter Block <hunterblock@Hunters-MacBook-Pro.local>
2020-07-16 16:44:53 -0400

    rentals

--- app.py
+++ app.py
@@ -167,6 +167,101 @@ def rentals_data():
 
     return rentals_df
 
+
+
+@app.route('/rentalpredict', methods=['GET', 'POST'])
+def rentalpredict():
+    if request.method == "POST":
+        # Redisplay page with warning for missing fields
+        missing = list()
+
+        for k, v in request.form.items():
+            if v == "":
+                missing.append(k)
+
+        if missing:
+            feedback = f"Missing fields for {', '.join(missing)}"
+            return render_template("index.html", feedback=feedback)
+
+        # Receive form inputs
+        age = int(request.form["age"])
+        sex = request.form["sex"]
+        race = request.form["race"]
+        city = request.form["city"].strip()
+        
+        # Transform inputs into array for ML model
+        if age<25:
+            array_age = [0,0]
+        elif age>54:
+            array_age = [0,1]
+        else:
+            array_age = [1,0]
+
+        if race == 'asian':
+            array_race = [0,0,0]
+        elif race == 'black':
+            array_race = [1,0,0]
+        elif race == 'hispanic':
+            array_race = [0,1,0]
+        else:
+            array_race = [0,0,1]
+
+        if sex == 'male':
+            array_sex = [0]
+        else:
+            array_sex = [1]
+
+        features = np.array([array_age + array_race + array_sex])
+                
+        # Call ML model
+        prediction = model.predict(features)
+
+        income = np.round(prediction[0], 2)
+        income2 = np.round((income * 52), 2)
+        income_wkly = ' '.join(map(str, income))
+        income_yrly = ' '.join(map(str, income2))
+
+        income3 = str(income_wkly).lstrip('[').rstrip(']')
+        
+        # Calculate max mortgate payment based on 4.3 average weeks in a month
+        can_afford = income * 4.3 * 0.28
+   
+        sql = """
+        SELECT *
+        FROM rental_data
+        """
+        datas = pd.read_sql(sql, con=connection)
+        datas["RegionName"] = datas["RegionName"].str.strip()
+        data2 = datas[["RegionName", "05-2020"]]
+    
+        tryagain = request.form["city"].strip()
+        
+        new = data2[data2['RegionName']==tryagain]
+       
+        value = new.iloc[0]["05-2020"]
+
+        # NEED TO PULL MEDIAN VALUE FROM ZILLOW FILE - placeholder for now
+        rent = value
+        rent_perc = (income * 4.3) * .3
+
+        if rent > rent_perc:
+            decision = "CAN NOT"
+        elif rent <= rent_perc:
+            decision = "CAN"
+
+        # Return website text
+        return render_template('rentals.html',\
+            summary_text=f"For a {age}-year-old {race} {sex}",\
+            prediction_text=f"the Estimated Income is: ${income3} per week (${income_yrly} per year)",\
+            decision_text=f"You {decision}  afford to rent in {city}",\
+            age_text=age,\
+            race_text=race,\
+            sex_text=sex
+            #decision_text=f"value {new}",\
+            #test_text= f'these are the columns {house_price} kajsdhfkjhakjhf {mortgage}'
+        )
+  
+
 # turn off for AWS deployment
 if __name__ == "__main__":
     app.run(debug=True)
